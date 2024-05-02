d7bb12e60fc362735ce8db9ade9b2f8c233915f2
Sonal Dev <78749573+Dev-Sonal@users.noreply.github.com>
2024-05-03 00:27:34 +0530

    Time: 0 ms (100%), Space: 7.4 MB (78.17%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -4,8 +4,11 @@ public:
         int x=haystack.size(),y=needle.size();
         if (y>x) return -1;
         for(int i=0;i<=x-y;i++){
-            string s=haystack.substr(i,y);
-            if(s==needle) return i;
+            int j=0;
+            while(j<y && haystack[i+j]==needle[j]){
+                ++j;
+            }
+            if(j==y) return i;
         }return -1;
     }
 };
