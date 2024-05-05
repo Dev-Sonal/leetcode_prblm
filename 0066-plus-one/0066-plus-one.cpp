5112936d404001d02235fe1248190c20812c6d83
Sonal Dev <78749573+Dev-Sonal@users.noreply.github.com>
2024-05-05 23:21:34 +0530

    Time: 0 ms (100%), Space: 10.2 MB (54.04%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> plusOne(std::vector<int>& digits) {
+        int n = digits.size();
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+        // If we're here, it means all digits were 9, so we need to add a new digit
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
