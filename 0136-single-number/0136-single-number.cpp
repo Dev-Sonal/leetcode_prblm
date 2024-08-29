0d881c6c94b87bdc4ac6dcc34de39c21f89a66fa
Sonal Dev <78749573+Dev-Sonal@users.noreply.github.com>
2024-08-29 11:36:15 +0530

    Time: 11 ms (82.23%), Space: 19.6 MB (61.83%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int reg=0;
-        for(int i=0;i<nums.size();i++){
-            reg=reg^nums[i];
+        int cnt=0;
+        for(auto x:nums){
+            cnt^=x;
         }
-        return reg;
+        return cnt;
+        
     }
 };
