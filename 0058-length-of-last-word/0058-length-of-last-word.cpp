080cd83379d32338005d69bc788dc19a945de5ee
Sonal Dev <78749573+Dev-Sonal@users.noreply.github.com>
2024-09-11 23:58:29 +0530

    Time: 0 ms (100%), Space: 7.8 MB (87.14%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -3,7 +3,7 @@ public:
     int lengthOfLastWord(string s) {
         int n=s.size();
         int cnt=0;
-        for (int i=n-1;i>0;i--){
+        for (int i=n-1;i>=0;i--){
             if (cnt>0 && s[i]==' '){
                 break;
             }
